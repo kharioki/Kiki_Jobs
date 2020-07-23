04412371fdc536859d5a3dd6813a7f0e63658d6c
kharioki <antolisah@gmail.com>
2020-07-23 03:11:35 +0300

    using bootstrap cards and badges

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
 
+import Job from './components/job';
+
 function App() {
-  const { jobs, loading, error } = useFetchJobs();
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
 
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error, try refreshing.</h1>}
 
-      <h1>{jobs.length}</h1>
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />;
+      })}
     </Container>
   );
 }
